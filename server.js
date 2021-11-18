56303e65a2873a44c3901f4e2f0e1a06e0d9ce2f
jaylen  hall <jaylenhall1@gmail.com>
2021-11-18 12:31:39 -0600

    idk what to call this

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require("path");
+const cors = require ("cors");
+
+const app = express();
+
+const port = process.env.PORT || 4545;
+
+app.listen(port, () => {
+    console.log(`theyre taking the martians to ${port}`);
+});
